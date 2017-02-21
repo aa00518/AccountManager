73c4e9ed7f03dd8bef33ab704887635c26187a96
Joe Banach <jbanach@silganmfg.com>
2017-02-21 16:40:41 -0600

    New stuff.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,24 @@ import { MyApp } from './app.component';
 import { TransactionsPage } from '../pages/transactions/transactions';
 import { EditTransactionPage } from '../pages/edittransaction/edittransaction';
 import { SettingsPage } from '../pages/settings/settings';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 import { Auth } from '../providers/auth';
 import { Accounts } from '../providers/accounts';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC18FXK353nkslSfU2fkMp2_XKIGQ1APsQ",
+  authDomain: "acctmgrfire.firebaseapp.com",
+  databaseURL: "https://acctmgrfire.firebaseio.com",
+  storageBucket: "acctmgrfire.appspot.com",
+  messagingSenderId: "658095225206"
+};
+
+export const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Popup,
+  remember: 'default',
+  scope: ['email']
+}
 
 @NgModule({
   declarations: [
@@ -15,7 +31,8 @@ import { Accounts } from '../providers/accounts';
     SettingsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
